0d9f5b3b2c38cdaacf8fa7a3631ac550ed02125c
Felipe Roberto <56833987+fekzim@users.noreply.github.com>
2022-04-28 14:26:15 -0300

    Created the Input style
    small style for the button and input :)

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
+import './Input.css';
 
 export default function Input(props) {
      const [text, setText] = useState("");
 
   return (
-    <div className="input">
-        <input type="text" placeholder='Adicione um item' value={text} onChange={(e)=> setText(e.target.value)}></input>
-        <button onClick={()=>props.todoAdd(text)}>+++</button>
+    <div className="Container">
+        <input type="text" placeholder='Adicione um item' value={text} onChange={(e)=> setText(e.target.value)}
+        id="textInput"></input>
+        <button onClick={()=>props.todoAdd(text)} id="addButton">+++</button>
     </div>
     // 
   )
